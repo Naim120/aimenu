aa0c896f09e19f85282deb449822a1087e28a5d3
sk naim <sknaimuddin234@gmail.com>
2023-08-03 04:04:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ function showInfo(overlayId) {
   }
 }
 
-function hideInfo(bigoverlayId) {
-  var bigoverlay = document.getElementById(bigoverlayId);
-  if (bigoverlay) {
-    bigoverlay.classList.remove("open");
+function hideInfo(overlayId) {
+  var overlay = document.getElementById(overlayId);
+  if (overlay) {
+    overlay.classList.remove("open");
   }
 }
 
+function closeOverlay() {
+  var overlay = document.querySelector('.bigoverlay.open');
+  if (overlay) {
+    overlay.classList.remove('open');
+  }
+}
+
+document.querySelector('.big-button').addEventListener('click', closeOverlay);
